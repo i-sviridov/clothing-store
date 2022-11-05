314846318a3a687e59c1808bc214c89a781dcb66
Ilya Sviridov <ilya.sviridov@yandex.ru>
2022-11-05 10:23:00 +0300

    adjusted environmental variables

--- next.config.js
+++ next.config.js
@@ -1,9 +1,19 @@
-module.exports = {
-  reactStrictMode: true,
-  env: {
-    mongodb_username: 'Develop_user',
-    mongodb_password: 'Develop_pass',
-    mongodb_cluster: 'cluster0',
-    mongodb_database: 'clothing-store',
-  },
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      env: {
+        mongodb_username: 'Develop_user',
+        mongodb_password: 'Develop_pass',
+        mongodb_cluster: 'cluster0',
+        mongodb_database: 'clothing-store',
+      },
+      reactStrictMode: true,
+    };
+  }
+
+  return {
+    reactStrictMode: true,
+  };
 };
